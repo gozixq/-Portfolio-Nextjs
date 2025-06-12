a50c70a972658d5da082335e2a2c151e45109493
Jejiezz <90750405+gozixq@users.noreply.github.com>
2025-06-12 09:21:27 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,21 @@
-import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
-import "./globals.css";
-import Navbar from "./components/navbar";
-import SplashCursor from "./components/ui/SplashCursor";
+// app/layout.tsx
+import Script from 'next/script';
+import type { Metadata } from 'next';
+import { Poppins } from 'next/font/google';
+import './globals.css';
+import Navbar from './components/navbar';
+import SplashCursor from './components/ui/SplashCursor';
+
 const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["100", "200", "400", "700"],
-  display: "swap",
-  variable: '--font-poppins', 
+  subsets: ['latin'],
+  weight: ['100', '200', '400', '700'],
+  display: 'swap',
+  variable: '--font-poppins',
 });
 
-
 export const metadata: Metadata = {
-  title: "Portfolio",
-  description: "Awesome app using Next.js",
+  title: 'Portfolio',
+  description: 'Awesome app using Next.js',
 };
 
 export default function RootLayout({
@@ -23,7 +25,28 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark">
+      <head>
+        <Script
+          id="gtm-head"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WDT5BV6M');`,
+          }}
+        />
+      </head>
       <body className={`${poppins.variable} ${poppins.className} font-sans`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WDT5BV6M"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <SplashCursor />
         <Navbar />
         {children}
